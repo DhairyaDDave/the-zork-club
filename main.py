32fccfa38fdc944a81169b0f2047616032ad75bf
DhairyaDDave <dhairya.dave@education.nsw.gov.au>
2026-05-07 13:51:15 +1000

    Started project

---
+++ main.py
@@ -0,0 +1,36 @@
+import 
+
+class Player:
+    def __init__(self, health, defense, curentPos, weapon, )
+    
+class GameObjects:
+    def __init__(self, name, description):
+        self.name = name
+        self. description = description
+
+    
+
+
+
+class Object_Interactives:      #work on this later to properly generalise consumables, weapons, 
+    def __init__(self, )
+
+
+class Weapons(GameObjects):
+    def __init__(self, durability, damage_inflected, status, )
+
+
+class Location(GameObjects):
+    def __init__(self, exits, items, characters, currentPos):
+        self.exits = {}
+        self.items = []
+        self.characters = []
+
+    def 
+        
+
+    
+def move():
+    .split
+
+
