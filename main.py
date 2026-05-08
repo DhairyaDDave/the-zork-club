259b4920cf91ad30ee06798cfd3c7415b7788aff
DhairyaDDave <dhairya.dave@education.nsw.gov.au>
2026-05-08 10:48:39 +1000

    Started adding Player functions and story

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import 
+
 
 class Player:
     def __init__(self, health, defense, curentPos, weapon, )
     
-class GameObjects:
+class GameObject:
     def __init__(self, name, description):
         self.name = name
         self. description = description
@@ -12,24 +12,22 @@ class GameObjects:
 
 
 
-class Object_Interactives:      #work on this later to properly generalise consumables, weapons, 
+class Object_Interactive(GameObject):      #work on this later to properly generalise consumables, weapons, 
     def __init__(self, )
 
 
-class Weapons(GameObjects):
+class Weapons(Object_Interactive):
     def __init__(self, durability, damage_inflected, status, )
 
 
-class Location(GameObjects):
+class Location(GameObject):
     def __init__(self, exits, items, characters, currentPos):
         self.exits = {}
         self.items = []
         self.characters = []
 
     def 
-        
-
-    
+            
 def move():
     .split
 
